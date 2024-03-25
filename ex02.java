0040db0990ad0d64421c7efb60943542f4ae0279
Auto Logon <autologon@CSED.LAB>
2024-03-25 11:16:17 -0300

    exercicio2

--- ex02.java
+++ ex02.java
@@ -1,26 +1,29 @@
-//Escreva um algoritmo que leia dois números digitados pelo usuário e exiba oresultado da sua soma.
+// Escreva um algoritmo que leia dois números digitados pelo usuário e exiba
+// oresultado da sua soma.
 
-import java.util.Scanner;
-
-public class ex02 {
-    public static void executar() {
-
-        Scanner scaneamento = new Scanner(System.in);
-
-        System.out.println("digite um número");
-
-        int sapo = scaneamento.nextInt();
-
-        System.out.println("digite um número");
-
-        int sapo2 = scaneamento.nextInt();
-
-        int resultado = sapo + sapo2;
-
-        System.out.println("O resultado é: " + resultado);
-
-        scaneamento.close();
-
-    }
-
-}
+/*
+ * import java.util.Scanner;
+ * 
+ * public class ex02 {
+ * public static void executar() {
+ * 
+ * Scanner scaneamento = new Scanner(System.in);
+ * 
+ * System.out.println("digite um número");
+ * 
+ * int sapo = scaneamento.nextInt();
+ * 
+ * System.out.println("digite um número");
+ * 
+ * int sapo2 = scaneamento.nextInt();
+ * 
+ * int resultado = sapo + sapo2;
+ * 
+ * System.out.println("O resultado é: " + resultado);
+ * 
+ * scaneamento.close();
+ * 
+ * }
+ * 
+ * }
+ */
--- programa.java
+++ programa.java
@@ -1,8 +1,8 @@
 public class programa {
     public static void main(String[] args) {
-        // 2
-        // ex01.executar();
+        //
+        ex01.executar();
         // ex02.executar();
-        ex03.executar();
+        // ex03.executar();
     }
 }
